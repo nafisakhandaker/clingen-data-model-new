ba10a7ff6832b2bb62b1f5e02f2bd27b3a380992
Nafisa Khandaker <nkhandaker@partners.org>
2017-07-20 13:06:36 -0400

    config change

--- config.rb
+++ config.rb
@@ -66,7 +66,6 @@ end
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
-set :layout_dir, '/static-resource/layouts/'
 
 # Build-specific configuration
 configure :build do
